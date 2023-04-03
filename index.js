7a9f73ba01a8dfb787e85346301ce49eade8d954
Rufi <68145072+Rufi-git@users.noreply.github.com>
2023-04-03 11:47:00 +0200

    asdsad

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
-const express = require("express")
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const todoRoutes = require("./routes/todo")
+const cors = require("cors");
+
+//express app
 const app = express();
-app.listen(4000, ()=>{
-    console.log("I am in port 4000")
-})
 
+//middleware
+app.use(express.json())
+app.use(cors());
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
 app.get("/",(req,res)=>{
-    res.json("Hello i succesffully deployed wuhuuu")
+    res.json("WUHUUUU!!!!!")
 })
+//routes
+app.use('/api/todo', todoRoutes)
 
-app.get("/home",(req,res)=>{
-    res.json("I am home")
-})
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+        //listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log("connected to db & listening on port", process.env.PORT)
+        })
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
 
-module.exports = app;
+    module.exports = app;
+
